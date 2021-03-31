ae82ef16b39e5c0e24fd354315b31f359d91836f
Antonio Bruno <antonio.bruno@external.t-systems.com>
2021-03-31 15:16:21 -0300

    Entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int PedidoId { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UserId { get; set; }
+        /// <summary>
+        /// Pedido deve ter pelo menos um pedido ou muitos pedidos
+        /// </summary>
+        public ICollection<ItemPedido>  ItensPedido { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        [StringLength(100)]
+        public string Descricao { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Preco { get; set; }
+    }
+}
